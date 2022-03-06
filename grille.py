3a8e757069922a6ace01b7d24dac87d8cbc314a6
frteynierndb <91475006+frteynierndb@users.noreply.github.com>
2022-03-06 21:26:32 +0100

    Update grille.py

--- grille.py
+++ grille.py
@@ -1,4 +1,4 @@
-# Créé par francois, le 05/03/2022 en Python 3.7
+
 https://openclassrooms.com/forum/sujet/coordonnees-de-grille-tkinter
 lvl1 = [l1, l2, l3, l4, l5, l6, l7, l8, l9]
 
@@ -14,23 +14,23 @@ l9 = [" ", 6, " ", " ", 7, " ", " ", " ", " "]
 
 def generateur(lvl1):
     for i in l1:
-        Button(main, text= i, borderwidth=5).grid(row=1, column=l1[i]+1)
+        Button(fenetre, text= i, borderwidth=5).grid(row=1, column=l1[i]+1)
     for i in l2:
-        Button(main, text= i, borderwidth=5).grid(row=2, column=l2[i]+1)
+        Button(fenetre, text= i, borderwidth=5).grid(row=2, column=l2[i]+1)
     for i in l3:
-        Button(main, text= i, borderwidth=5).grid(row=3, column=l3[i]+1)
+        Button(fenetre, text= i, borderwidth=5).grid(row=3, column=l3[i]+1)
     for i in l4:
-        Button(main, text= i, borderwidth=5).grid(row=4, column=l4[i]+1)
+        Button(fenetre, text= i, borderwidth=5).grid(row=4, column=l4[i]+1)
     for i in l5:
-        Button(main, text= i, borderwidth=5).grid(row=5, column=l5[i]+1)
+        Button(fenetre, text= i, borderwidth=5).grid(row=5, column=l5[i]+1)
     for i in l6:
-        Button(main, text= i, borderwidth=5).grid(row=6, column=l6[i]+1)
+        Button(fenetre, text= i, borderwidth=5).grid(row=6, column=l6[i]+1)
     for i in l7:
-        Button(main, text= i, borderwidth=5).grid(row=7, column=l7[i]+1)
+        Button(fenetre, text= i, borderwidth=5).grid(row=7, column=l7[i]+1)
     for i in l8:
-        Button(main, text= i, borderwidth=5).grid(row=8, column=l8[i]+1)
+        Button(fenetre, text= i, borderwidth=5).grid(row=8, column=l8[i]+1)
     for i in l9:
-        Button(main, text= i, borderwidth=5).grid(row=9, column=l9[i]+1)
+        Button(fenetre, text= i, borderwidth=5).grid(row=9, column=l9[i]+1)
 
 lvl1c = [l1c, l2c, l3c, l4c, l5c, l6c, l7c, l8c, l9c]
 
